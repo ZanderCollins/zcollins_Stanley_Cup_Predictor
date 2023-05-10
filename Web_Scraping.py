eb8998b9a5955da0e3d53072e131b4ee26785523
ZanderCollins <126900801+ZanderCollins@users.noreply.github.com>
2023-05-10 10:20:39 -0700

    progress

--- Web_Scraping.py
+++ Web_Scraping.py
@@ -1,21 +1,34 @@
-# Made by Zander Collins
-# Help from https://www.zenrows.com/blog/speed-up-web-scraping-with-concurrency-in-python#concurrency
-# Web scapper setup 
 
-import pandas as pd
-from bs4 import BeautifulSoup
+############## open website with selenium and webdriver ###################
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path='C:/Users/Zander.Collins23/Downloads/chromedriver_win32/chromedriver.exe')
-driver.get('https://www.nhl.com/standings/2022/league')
-results = []
-content = driver.page_source
-soup = BeautifulSoup(content)
+# URL = 'https://herenow.com/results/#/races/20899/results'
+URL = 'https://www.nhl.com/standings/2022/league'
 
-for element in soup.findALL(attrs='team--name'):
-    name = element.find('col-9 row 0')
-    if name not in results: 
-        results.append(name.text)
+chrome_driver = "Code\Projects\chromedriver.exe"
+driver = webdriver.Chrome(chrome_driver)
+driver.get(URL)
+sleep(10)
+# spans = driver.find_element(By.TAG_NAME, "span")
+# print(type(spans))
+searchresults = driver.find_elements(By.XPATH,"//span[contains(@class,'team--name')]")
+print(searchresults[0].text)
+print(searchresults[1].text)
+print(searchresults[2].text)
+print(searchresults[3].text)
+print(searchresults[4].text)
+print(searchresults[5].text)
+print(searchresults[4].text)
 
-print(results)
+# datafound = []
 
+# for i in searchresults:
+#     if len(i.text) > 0:
+#         datafound.append(i.text)
+#     if len(datafound) > 45:
+#         break
+
+# print(datafound)
